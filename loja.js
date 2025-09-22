ec031e4e3bc762a88a38e9f7508087ddcbbb4496
nxksnyx <niksdaora@gmail.com>
2025-09-22 13:04:48 -0300

    Deploy autom├ítico em 22/09/2025 13:04:47,71

--- loja.js
+++ loja.js
@@ -26,6 +26,11 @@ const produtosColetivos = [
 ];
 
 const produtosInfo = {
+  aula_jogos: { nome: "Aula com jogos e brincadeiras na área externa", imagem: "https://i.imgur.com/rRKueT4.jpeg" },
+  aula_ping: { nome: "Aula com jogos e brincadeiras no pátio", imagem: "https://i.imgur.com/o48S1TL.jpeg" },
+  aula_fisica: { nome: "Aula extra de educação física", imagem: "https://jundiai.sp.gov.br/noticias/wp-content/uploads/sites/32/2018/11/emeb_marcos_gasparian-166.jpg" },
+  aula_cinema: { nome: "Aula de Cinema com Pipoca na sala", imagem: "https://2.bp.blogspot.com/-goes4lu3wsQ/VTl7mzY2lpI/AAAAAAAAGR0/By2X6yTVvZk/s1600/crian%C3%A7as-filmes.jpg" },
+  aula_horta: { nome: "Aula na Horta", imagem: "https://i.imgur.com/EDqWAVf.jpeg" },
   teclado_mouse: { nome: "Teclado e Mouse Maxtro", imagem: "https://i.imgur.com/UBqi1ni.jpeg" },
   mouse_gamer: { nome: "Mouse Gamer", imagem: "https://i.imgur.com/kpcOYTX.jpeg" },
   kit_bobbie: { nome: "Kit copo bobbie goods e lápis de cor", imagem: "https://i.imgur.com/THsU3gP.jpeg" },
@@ -35,11 +40,6 @@ const produtosInfo = {
   kit_doces: { nome: "Kit doces", imagem: "https://i.imgur.com/rpIGmVB.jpeg" },
   fini: { nome: "Fini", imagem: "https://i.imgur.com/8Nwj3tF.jpeg" },
   pipoca: { nome: "Pipoca Doce", imagem: "https://i.imgur.com/3mn1jWo.jpeg" },
-  aula_jogos: { nome: "Aula com jogos e brincadeiras na área externa", imagem: "https://i.imgur.com/rRKueT4.jpeg" },
-  aula_ping: { nome: "Aula com jogos e brincadeiras no pátio", imagem: "https://i.imgur.com/o48S1TL.jpeg" },
-  aula_fisica: { nome: "Aula extra de educação física", imagem: "https://jundiai.sp.gov.br/noticias/wp-content/uploads/sites/32/2018/11/emeb_marcos_gasparian-166.jpg" },
-  aula_cinema: { nome: "Aula de Cinema com Pipoca na sala", imagem: "https://2.bp.blogspot.com/-goes4lu3wsQ/VTl7mzY2lpI/AAAAAAAAGR0/By2X6yTVvZk/s1600/crian%C3%A7as-filmes.jpg" },
-  aula_horta: { nome: "Aula na Horta", imagem: "https://i.imgur.com/EDqWAVf.jpeg" },
   jogos_virtuais: { nome: "Aula com Jogos Virtuais", imagem: "https://img.freepik.com/fotos-gratis/criancas-da-escola-usando-tablet-digital-em-sala-de-aula_107420-57955.jpg" },
   harry_potter: { nome: "Harry Potter e a Ordem da Fênix", imagem: "https://i.imgur.com/u39XPhX.jpeg" },
   narizinho: { nome: "Reinações de Narizinho", imagem: "https://i.imgur.com/XKfNUFl.jpeg" },
